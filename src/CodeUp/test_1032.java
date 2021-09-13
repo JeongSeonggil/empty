e6748e01a76db60fe574f71506d4db888e6ebb8e
JeongSeongGil <dataofsg02@gmail.com>
2021-09-13 23:14:10 +0900

    test

---
+++ src/CodeUp/test_1032.java
@@ -0,0 +1,11 @@
+package CodeUp;
+
+import java.util.Scanner;
+public class test_1032 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int value = sc.nextInt();
+		System.out.printf("%x", value);
+	}
+
+}
