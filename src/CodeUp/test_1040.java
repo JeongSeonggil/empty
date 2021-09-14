2724ef4cf46245f3862b7c67c95119151eda9227
JeongSeongGil <dataofsg02@gmail.com>
2021-09-14 17:40:56 +0900

    2021-09-14

---
+++ src/CodeUp/test_1040.java
@@ -0,0 +1,11 @@
+package CodeUp;
+
+import java.util.Scanner;
+
+public class test_1040 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.printf("%d", sc.nextLong() + sc.nextLong());
+
+	}
+}
---
+++ src/CodeUp/test_1041.java
@@ -0,0 +1,11 @@
+package CodeUp;
+
+import java.util.Scanner;
+
+public class test_1041 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		char value = sc.nextLine().charAt(0);
+		System.out.println((value += 1));
+	}
+}
