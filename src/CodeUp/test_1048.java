2a15059854a4c0e061bbda5ba0e6cc163d9dfcd8
JeongSeongGil <dataofsg02@gmail.com>
2021-09-19 19:39:25 +0900

    commit

---
+++ src/CodeUp/test_1048.java
@@ -0,0 +1,13 @@
+package CodeUp;
+
+import java.util.Scanner;
+
+public class test_1048 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		System.out.printf("%d\n", a <<b);
+
+	}
+}
