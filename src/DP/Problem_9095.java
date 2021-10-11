3a61722f0373ae70a5a88261725817c64c0938c8
JeongSeongGil <dataofsg02@gmail.com>
2021-10-11 19:08:13 +0900

    commit

---
+++ src/DP/Problem_9095.java
@@ -0,0 +1,25 @@
+package DP;
+
+import java.util.Scanner;
+public class Problem_9095 {
+    static int[] dp = new int[11];
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 4;
+
+        for (int i = 4; i <= 10; i ++){
+            dp[i] = dp[i - 3] + dp[i - 2] + dp[i - 1];
+        }
+        int count = sc.nextInt();
+        for (int i = 0; i < count; i++){
+            int value = sc.nextInt();
+            System.out.println(dp[value]);
+        }
+
+    }
+
+}
